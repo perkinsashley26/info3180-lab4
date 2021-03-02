5fdde54abfacad729b23b5239bf13ee61d56b1d9
Ashley perkins <perkinsashley26@gmail.com>
2021-03-01 23:07:25 -0500

    Exercie 1 completed

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField,TextAreaField
+from wtforms.validators import DataRequired
+
+class UploadForm(FlaskForm):
+    uploadfile=FileField('Image Upload', validators=[FileRequired(),FileAllowed(['jpg','png'],'Images Only')])
--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ import os
 from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
+from app.forms import UploadForm
 
 
 ###
@@ -32,15 +33,20 @@ def upload():
         abort(401)
 
     # Instantiate your form class
+    formupload=UploadForm()
 
     # Validate file upload on submit
     if request.method == 'POST':
-        # Get file data and save to your uploads folder
-
-        flash('File Saved', 'success')
+        if formupload.validate_on_submit():
+            # Get file data and save to your uploads folder
+            file=request.files['uploadfile']
+            filename=secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            
+            flash('File Saved', 'success')
         return redirect(url_for('home'))
 
-    return render_template('upload.html')
+    return render_template('upload.html', form=formupload)
 
 
 @app.route('/login', methods=['POST', 'GET'])
